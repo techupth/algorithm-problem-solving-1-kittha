dd01e7d1665edc3c74c8f81e1daaf31f6da5a6fb
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-07-10 14:21:00 +0700

    finished assignment

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  let pos = [];
+  let i = 0;
+  let j = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        pos.push(i);
+        pos.push(j);
+      }
+    }
+  }
+  return pos;
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 7, 11, 15], 23));
