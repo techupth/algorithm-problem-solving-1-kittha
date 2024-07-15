56d54a96f8141905c37ad46a2a4f6c3a5e755807
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-07-15 14:02:07 +0700

    hotfix :3

--- ex-1.js
+++ ex-1.js
@@ -1,16 +1,12 @@
 function twoSum(numbers, target) {
-  let pos = [];
-  let i = 0;
-  let j = 0;
   for (let i = 0; i < numbers.length; i++) {
-    for (let j = 1; j < numbers.length; j++) {
+    for (let j = i + 1; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] === target) {
-        pos.push(i);
-        pos.push(j);
+        return [i, j];
       }
     }
   }
-  return pos;
+  return [];
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
